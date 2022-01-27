e114ad438620399e91d5687f8e75e1d18e6cdb7b
Ruslan <rjabari@stanford.edu>
2022-01-27 09:32:02 -0800

    demoed in expo introduction

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 24}}>Hello CS47, again!</Text>
       <StatusBar style="auto" />
     </View>
   );
